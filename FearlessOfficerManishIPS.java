dc057209003dc164bacf9b685e3f9abe7997c3c7
Manish30092005 <manish.marts@gmail.com>
2025-03-07 14:33:39 +0530

    Create FearlessOfficerManishIPS.java

---
+++ FearlessOfficerManishIPS.java
@@ -0,0 +1,47 @@
+class PersonalLifeOfManishIPS {
+    void display() {
+        String born = "30th September 1977 in Chennai, Tamil Nadu.";
+        System.out.println("The Intelligent and Fearless, yet aggressive Police of India was none other than Assistant Superintendant of Police, Manish M. He was born on " + born);
+        String schooling = "Modern Senior Secondary School, Nanganallur, Chennai.";
+        System.out.println("He did his schooling in " + schooling);
+        String collegeLife = "Computer Science and Engineering in Sathyabama Institute Of Science and Technology, Chennai.";
+        System.out.println("He pursued his undergraduate and postgraduate studies in " + collegeLife);
+        String pgLife = "Legistlative Laws in IIT Kharagpur, West Bengal";
+        System.out.println("He further pursued postgraduate studies and mastered in " + pgLife);
+        String IPSTrainingLife = "2 Years in Nagpur, 1 Year in Chennai.";
+        System.out.println("He further pursued IPS Training for " + IPSTrainingLife);
+    }
+}
+
+class ProfessionalLifeOfManishIPS {
+    void display() {
+        String job = "Police Officer";
+        System.out.println("After his 3 years of IPS Training, He joined as a " + job + "in Chennai and worked as a constable for a year. While Working as a Constable, he catched the Child-Trafficking Network all over India through his Intelligence. So He got Promoted as an Inspector. While Working as an Inspector, He Catched The Drug Mafia by posing as his Subordinate and using Bugs and Spy Cameras, he gathered all informations and reported to his higher authorities. After his Higher authorities gave a green signal to catch him, Manish lured the drug mafia to Krishnapatnam Port, Andhra Pradesh. While the Drug Mafia attempts to sell drugs to abroad, Manish and His Subordinates caught the Drug Mafia red-handedly. He Stopped The drugs production and selling not only to other Indian states but also abroad. He was awarded as the 'Best" + job + "'Of The Nation' by the Prime Minister and The President Of the India. He Got Promoted as the Assistant Commisioner Of Police. During His tenure as an ACP, He got transferred to Tiruchendur.");
+        String skills = "Intelligent; Aggressive; Saviour to the General Public; Ruthless Nightmare to the Criminals; Problem-Solving; Rigorous Trainer; Mentally and Physically Strong; Deep Knowledge of Computers, Laws and Society; Sincerity to his Duties and his Higher Authorities.";
+        System.out.println("He was proficient in " + skills);
+    }
+}
+
+class TiruchendurMission {
+    void display() {
+        System.out.println("Soundirapandi was a Don in Tiruchendur. He has a Son named Muthupandi IPS, who was a corrupt Inspector. Muthupandi and Soundirapandi was considered as a Poisonous Plant Of The Society. Sakthivel is an MLA of Tiruchendur. He found and runs a best school in Kanchipuram named, Siragugal School. He was the best correspondent of the School. He has a son named Gunasekar (Guna). Sakthivel and Guna Opposed Soundirapandi and Muthupandi's tactics against the society. Soundirapandi Orders Muthupandi to kill an old man and kidnap a girl for Opposing and filing a complaint against them. Upon Soundirapandi's orders, Muthupandi does so. On early morning, Guna and Sakthivel suspect Muthupandi and Soundirapandi's involvement in this act. So they file a complaint to Manish against Pandi and Sons. Upon Learning this, Muthupandi kills Sakthivel. Before dying, Guna makes a promise to his Father Sakthivel to run Siragugal School in a best way. Guna is Distraught over his Father's death. He retaliates by luring Soundirapandi to latter's house, Guna burns Soundirapandi along his house. Upon learning this, Muthupandi attempts to kill Guna but is saved by Manish. The Latter arrests Muthupandi and takes him to Palayamkottai Jail. Manish Plans with Jail inmates to kill Muthupandi, instilling fear to Muthupandi. After planning, Manish leaves with his family to Singapore for Vacations. The Jail Inmates attempted to kill Muthupandi, but instead, they got killed by Muthupandi. Christopher Sivakumar helps him to be ruthless by training him and changing his identity to Santhosh Kumar, a disciplinarian Maths Teacher. Santhosh Kumar (Formerly Muthupandi IPS) joins Siragugal School, Kanchipuram. He was Employed By The Cruel Principal Bhairavan. Manish Got Transferred to Kanchipuram.");
+    }
+}
+class KanchipuramMission {
+    void display() {
+        System.out.println("While he got transferred in Kanchipuram, Guna informs Manish about the atrocities happened in the Modern School which is located 5 kms away from The Siragugal School. So he posed as a teacher in the Modern School and gathered all informations against the school by fixing spy cameras with microphone. After gathering evidences against Naachiyappan who was the Correspondent of the Modern School, Manish lures Naachiyappan to the latter's room and interrogates casually. Manish slaps Naachiyappan for refusing to answer his questions. While Manish records Naachiyappan in the Camera, Naachiyappan Reveals the truth. He sends to his higher authorities and news channels. Naachiyappan was arrested by Manish for denying his role as the mastermind behind detaining innocent school boys and girls and labeling them as unruly students to prevent them from studying at other schools. He also protected culpable students and teachers of the Modern School. Upon learning this, Manish publicly slapped Naachiyappan, humiliating him for his unethical business practices. While being escorted to court, Naachiyappan was shot dead by a Modern School student named Dharshini. Dharshini informed the court that she acted in self-defense due to his wrongful conduct. The court granted her bail, and the Modern School was subsequently shut down by Manish IPS.");
+    }
+}
+
+class FearlessOfficerManishIPS {
+    public static void main(String[] args) {
+        PersonalLifeOfManishIPS personalLife = new PersonalLifeOfManishIPS();
+        personalLife.display();
+        ProfessionalLifeOfManishIPS professionalLife = new ProfessionalLifeOfManishIPS();
+        professionalLife.display();
+        TiruchendurMission Tiruchendur = new TiruchendurMission();
+        Tiruchendur.display();
+        KanchipuramMission Kanchipuram = new KanchipuramMission();
+        Kanchipuram.display();
+    }
+}
